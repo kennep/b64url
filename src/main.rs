5a8b4557c5a5da28fdd045e8975260ee58d106a9
Kenneth Wang Pedersen <kenneth@wangpedersen.com>
2021-03-02 22:24:31 +0100

    Rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use structopt::StructOpt;
 use std::io::{self, Read, Write};
 use std::io::{Error, ErrorKind};
+use structopt::StructOpt;
 
 extern crate base64;
 
@@ -14,14 +14,12 @@ struct Cli {
     encode: bool,
 }
 
-fn encode<T: AsRef<[u8]>>(input: T) -> Vec<u8>
-{
+fn encode<T: AsRef<[u8]>>(input: T) -> Vec<u8> {
     let result = base64::encode_config(input, base64::URL_SAFE_NO_PAD);
     return result.as_bytes().to_vec();
 }
 
-fn decode<T: AsRef<[u8]>>(input: T) -> io::Result<Vec<u8>>
-{
+fn decode<T: AsRef<[u8]>>(input: T) -> io::Result<Vec<u8>> {
     let inputstr = std::str::from_utf8(input.as_ref())
         .map_err(|e| Error::new(ErrorKind::InvalidData, e.to_string()))?;
 
@@ -38,31 +36,41 @@ fn main() -> io::Result<()> {
         let result = Cli::clap().print_help();
         eprintln!("\n"); // print_help does not print a trailing newline.
         match result {
-            Ok(_) => return Err(Error::new(ErrorKind::Other, "Cannot use --encode and --decode at the same time!")),
-            Err(e) => return Err(Error::new(ErrorKind::Other, e.to_string()))
+            Ok(_) => {
+                return Err(Error::new(
+                    ErrorKind::Other,
+                    "Cannot use --encode and --decode at the same time!",
+                ))
+            }
+            Err(e) => return Err(Error::new(ErrorKind::Other, e.to_string())),
         }
     }
 
     let mut buffer = Vec::new();
     match io::stdin().read_to_end(&mut buffer) {
         Ok(_) => (),
-        Err(e) => return Err(e)
+        Err(e) => return Err(e),
     }
 
-    while match buffer.last() { Some(b) => *b == b'\r' || *b == b'\n', None => false }
-    {
+    while match buffer.last() {
+        Some(b) => *b == b'\r' || *b == b'\n',
+        None => false,
+    } {
         buffer.truncate(buffer.len() - 1);
     }
 
-    if opt.decode
-    {
+    if opt.decode {
         match decode(buffer) {
-            Ok(result) => return io::stdout().write_all(&result).and_then(|_| io::stdout().write_all(b"\n")),
-            Err(e) => return Err(Error::new(ErrorKind::Other, e.to_string()))
+            Ok(result) => {
+                return io::stdout()
+                    .write_all(&result)
+                    .and_then(|_| io::stdout().write_all(b"\n"))
+            }
+            Err(e) => return Err(Error::new(ErrorKind::Other, e.to_string())),
         }
-    }
-    else
-    {
-        return io::stdout().write_all(&encode(buffer)).and_then(|_| io::stdout().write_all(b"\n"));
+    } else {
+        return io::stdout()
+            .write_all(&encode(buffer))
+            .and_then(|_| io::stdout().write_all(b"\n"));
     }
 }
